ee3b9ba45ea45986a410861c2cd759dd1e0dd2c4
HarryCode15 <harshbtekade137@gmail.com>
2024-08-02 02:46:56 +0530

    Completed frontend, next functionality integration.

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ function App() {
         <div className='logo-box' >
           <img src="./Login_logo.svg" alt="login logo" className="logo" />
           <h2>Sign In Page For Test</h2>
-          <button>
+          <button className='btn-login-opt'>
           <img src="./google.png" alt="google" className='google-logo' />
           Sign In with Google
           </button>
 
-          <button>
+          <button className='btn-login-opt'>
           <img src="./facebook.png" alt="facebook" className='google-logo' />
           Sign In with Facebook
           </button>
 
           <hr/>
           <span>OR</span>
+
+          <form>
+            <input type='text' placeholder='Phone email or username' className='btn-next' />
+            <button className='btn-next'><span>Next</span></button>
+          </form>
         </div>
       </div>
     </div>
