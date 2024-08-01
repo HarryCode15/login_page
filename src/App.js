a5151af30a8e270adb75ead223254c10558cc85e
HarryCode15 <harshbtekade137@gmail.com>
2024-08-01 17:53:03 +0530

    Updated buttons, added a divider for multi login.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <div className='login-card'>
-      <h1>Hello User</h1>
+      <div className="login-card">
+        <div className='logo-box' >
+          <img src="./Login_logo.svg" alt="login logo" className="logo" />
+          <h2>Sign In Page For Test</h2>
+          <button>
+          <img src="./google.png" alt="google" className='google-logo' />
+          Sign In with Google
+          </button>
+
+          <button>
+          <img src="./facebook.png" alt="facebook" className='google-logo' />
+          Sign In with Facebook
+          </button>
+
+          <hr/>
+          <span>OR</span>
+        </div>
       </div>
     </div>
   );
